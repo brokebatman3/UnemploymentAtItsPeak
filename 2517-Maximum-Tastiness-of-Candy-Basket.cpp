03ef56a0f98d7f11f7369d3a2c4619a5e553cd89
BrokeBatman <brokebatman3@gmail.com>
2025-01-10 18:06:28 +0530

    [2517] [Time Beats: 18.16%] [Memory Beats: 59.74%] - LeetPush

--- 2517-Maximum-Tastiness-of-Candy-Basket.cpp
+++ 2517-Maximum-Tastiness-of-Candy-Basket.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& p, int minDist, int k) {
-        int take = 1;
-        int lastCowPosition = p[0];
-        for (int i = 1; i < p.size(); i++) {
-            if (p[i] - lastCowPosition >= minDist) {
-                take++;
-                lastCowPosition = p[i];
-                if (take >= k)
-                    return true;
+    bool check(int x, vector<int>& nums, int k) {
+
+        int j = 0;
+        int len = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] - nums[j] >= x) {
+                len++;
+                j = i;
             }
         }
-        return false;
+            if (len >= k) {
+                return 1;
+            }
+
+        return 0;
     }
-    int maximumTastiness(vector<int>& p, int k) {
-        int n = p.size();
-        sort(p.begin(), p.end());
-        int l = 1, h = p[n - 1] - p[0];
-        int ans = 0;
-        while (l <= h) {
-            int m = (l + h) / 2;
-            if (isPossible(p, m, k)) {
-                ans = m;
-                l = m + 1;
-            } else
-                h = m - 1;
+    int maximumTastiness(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int low = 0;
+        int high = nums[n - 1] - nums[0];
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            bool res = check(mid, nums, k);
+
+            if (res) {
+                ans = mid;
+                low = mid + 1;
+
+            } else {
+                high = mid - 1;
+            }
         }
         return ans;
     }
