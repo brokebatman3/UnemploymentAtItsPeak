7b12798db4f0cd90343efcc29c52e012fdfc0d13
BrokeBatman <brokebatman3@gmail.com>
2025-01-30 17:21:29 +0530

    [849] [Time Beats: 100.00%] [Memory Beats: 31.43%] - LeetPush

--- 849-Maximize-Distance-to-Closest-Person.cpp
+++ 849-Maximize-Distance-to-Closest-Person.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    int maxDistToClosest(vector<int>& nums) {
-        int n = nums.size(); 
-        map<int , pair<int ,int>> m ; 
-        for(int i = 0 ; i < n ; i++){
-            int j , k ; 
-            for( j = i-1 ; j >= 0 ; j--){
-                if(nums[j] != 0 ){
-                    break ; 
-                }
-            }
-
-            for(k = i+1 ; k <n ; k++){
-                if(nums[k] != 0 ){
-                    break ; 
-                }
-            }
-            if(k >=  n){
-                k = INT_MAX; 
-            }
-            if(j < 0 ){
-                j = INT_MAX; 
-            }
-            if(nums[i] == 0 ){
-
-            m.insert({i , {j , k }}); 
+    int maxDistToClosest(vector<int>& seats) {
+        int longest = 0, beginning = 0, i = 0;
+        // count zeros in beginning
+        while (seats[i]==0)
+        {
+            beginning++;
+            i++;
+        }
+        // find longest sequence of zeros in middle
+        int count = beginning;
+        for (; i<seats.size(); i++)
+        {
+            if (seats[i] == 0)
+                count++;
+            else
+            {
+                longest = std::max({count, longest});
+                count = 0;
             }
         }
-        
-        int diff = INT_MIN ; 
-        for(auto x : m ){
-            // cout<< x.first <<" " <<  x.second.first <<" "<<x.second.second <<endl;
-            int ld = abs(x.first - x.second.first);
-            int rd = abs(x.first - x.second.second);
-            cout<< ld <<" " << rd <<endl; 
-            if(min(ld, rd ) > diff){
-                diff = min(ld,rd); 
-            }
-        }
-        return diff ; 
-
+        // at the end, count = length of zeros in the end
+        // in the middle we have to divide the length in two to find maximum distance
+        longest = longest%2==0?longest/2:longest/2+1;
+        // return the largest distance
+        return std::max({longest, count, beginning});
     }
 };
