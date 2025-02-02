8e4f6a3e92864d36fcd9ff98c561a489499f8ec2
BrokeBatman <brokebatman3@gmail.com>
2025-02-02 13:34:33 +0530

    [572] [Time Beats: 44.73%] [Memory Beats: 55.95%] - LeetPush

---
+++ 572-Subtree-of-Another-Tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // If both nodes are NULL, they are identical
+        if (p == NULL && q == NULL) {
+            return true;
+        }
+        // If only one of the nodes is NULL, they are not identical
+        if (p == NULL || q == NULL) {
+            return false;
+        }
+        // Check if values are equal and recursively check left and right
+        // subtrees
+        if (p->val == q->val) {
+            return isSameTree(p->left, q->left) &&
+                   isSameTree(p->right, q->right);
+        }
+        // Values are not equal, they are not identical
+        return false;
+    }
+    bool func(TreeNode* root, TreeNode* sub) {
+        if (root == NULL && sub == NULL) {
+            return true;
+        }
+        // If only one of the nodes is NULL, they are not identical
+        if (root == NULL || sub == NULL) {
+            return false;
+        }
+        bool l = func(root->left , sub ); 
+        bool r = func(root->right , sub ); 
+        return isSameTree(root , sub ) || l || r ; 
+       
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        return func(root, subRoot);
+    }
+};
