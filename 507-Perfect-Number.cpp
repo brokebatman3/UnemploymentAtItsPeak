b1c1ff7d3abb892b171406372a3fb62016fb1d38
BrokeBatman <brokebatman3@gmail.com>
2025-02-25 16:59:07 +0530

    [507] [Time Beats: 25.66%] [Memory Beats: 88.13%] - LeetPush

---
+++ 507-Perfect-Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int ans = 0 ; 
+        for(int i = 1 ; i< num ; i++){
+            if(num % i == 0 ){
+                ans += i; 
+            }
+        }
+        if(ans == num ){
+            return 1;
+        }
+        return 0;
+    }
+};
