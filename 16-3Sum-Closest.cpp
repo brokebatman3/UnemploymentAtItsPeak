606c09e13673184f359015e065c4eb28da18bd1f
BrokeBatman <brokebatman3@gmail.com>
2025-02-26 18:27:37 +0530

    [16] [Time Beats: 63.33%] [Memory Beats: 56.17%] - LeetPush

--- 16-3Sum-Closest.cpp
+++ 16-3Sum-Closest.cpp
@@ -3,20 +3,26 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        int mini = INT_MAX; 
         int res = -1; 
+        int mini = INT_MAX; 
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    int temp = nums[i] + nums[j] + nums[k]; 
-                    if(abs(target - temp) < mini){
-                        mini = abs(target - temp); 
-                        res = temp ; 
-                    }
+            int start = i + 1;
+            int end = n - 1;
+            while (start < end) {
+                int temp = nums[i] + nums[start] + nums[end];
+                if (abs(temp - target) < mini) {
+                    mini = abs(temp - target) ;res = temp;
+                }
 
+                if (temp == target) {
+                    return temp;
+                } else if (temp > target) {
+                    end--;
+                } else {
+                    start++;
                 }
             }
         }
-        return res ;
+        return  res ;
     }
 };
