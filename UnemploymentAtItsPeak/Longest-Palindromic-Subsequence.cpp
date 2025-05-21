ff91a8fba8dba8d2c519b2ad55bef925ea543201
BrokeBatman <brokebatman3@gmail.com>
2025-05-21 10:00:24 +0530

    [516] [Time Beats: 84.21%] [Memory Beats: 85.78%] - LeetPush

--- UnemploymentAtItsPeak/Longest-Palindromic-Subsequence.cpp
+++ UnemploymentAtItsPeak/Longest-Palindromic-Subsequence.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int func(string& s1, string& s2, int i, int j) {
-        if (i == s1.size()) {
+int dp[1001][1001]; 
+int func(string &x, string &y, int i, int j) {
+        if (i < 0 || j < 0) {
             return 0;
         }
-        if (j == s2.size()) {
-            return 0;
+        if(dp[i][j] != -1){
+            return dp[i][j]; 
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        if (x[i] == y[j]) {
+            return dp[i][j] =  1 + func(x, y, i - 1, j - 1);
         }
-        if (s1[i] == s2[j]) {
-            return dp[i][j] = 1 + func(s1, s2, i + 1, j + 1);
-        } else {
-            return dp[i][j] = max(
-                       func(s1, s2, i + 1, j),
-                       max(func(s1, s2, i, j + 1), func(s1, s2, i + 1, j + 1)));
-        }
-    }
-    int longestCommonSubsequence(string s1, string s2) {
-        memset(dp, -1, sizeof(dp));
-
-        if (s1.size() > s2.size()) {
-
-            return func(s1, s2, 0, 0);
-        } else {
-            return func(s2, s1, 0, 0);
-        }
+        return dp[i][j] =  max(func(x, y, i - 1, j) , func(x , y , i , j - 1));
     }
     int longestPalindromeSubseq(string s) {
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
-        return longestCommonSubsequence(s, s2);
+        memset(dp , -1 , sizeof(dp));
+        string z = s;
+        reverse(z.begin() , z.end()); 
+        return func(s ,z , s.length()-1 , z.length()-1  ); 
     }
 };
