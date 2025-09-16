2e897dedaacacfad618737cd0be12913cc234113
BrokeBatman <brokebatman3@gmail.com>
2025-09-16 11:31:32 +0530

    Time: 4 ms (1.63%) | Memory: 17.9 MB (54.86%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
-    // bool search(vector<int>& nums, int target) {
-    //     if (nums.size() == 0) {
-    //         return 0;
-    //     }
-    //     int l = 0;
-    //     int h = nums.size() - 1;
-    //     while (l <= h) {
-    //         int mid = l + (h - l) / 2;
-    //         cout << mid << " ";
-    //         if (target == nums[mid]) {
-    //             return mid;
-    //         }
-    //         if (nums[l] == nums[mid] && nums[mid] == nums[h]) {
-    //             l++;
-    //             h--;
-    //             continue;
-    //         }
-    //         if (nums[l] <= nums[mid]) {
-    //             if (target < nums[mid] && target >= nums[l]) {
-    //                 h = mid - 1;
-    //             } else {
-    //                 l = mid + 1;
-    //             }
-    //         } else {
-    //             if (target > nums[mid] && target <= nums[h]) {
-    //                 l = mid + 1;
-    //             } else {
-    //                 h = mid - 1;
-    //             }
-    //         }
-    //     }
-    //     return 0;
-    // }
+    bool seahrch(vector<int>& nums, int target) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        if(nums.size() == 1){
+            return nums[0] == target;
+        }
+        int l = 0;
+        int h = nums.size() - 1;
+        while (l <= h) {
+            
+            int mid = l + (h - l) / 2;
+            cout << mid << " ";
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (nums[l] == nums[mid] && nums[mid] == nums[h]) {
+                l++;
+                h--;
+                continue;
+            }
+            if (nums[l] <= nums[mid]) {
+                if (target >= nums[l] && target < nums[mid] ) {
+                    h = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[h]) {
+                    l = mid + 1;
+                } else {
+                    h = mid - 1;
+                }
+            }
+        }
+        return 0;
+    }
     bool search(vector<int>& arr, int key) {
         int n = arr.size();
         if (n == 0) return false;
@@ -40,6 +44,7 @@ public:
         int l = 0, h = n - 1;
         while (l <= h) {
             int mid = l + (h - l) / 2;
+            cout << mid << " ";
             if (arr[mid] == key) return true;
 
             // Ambiguous case due to duplicates: shrink both sides
