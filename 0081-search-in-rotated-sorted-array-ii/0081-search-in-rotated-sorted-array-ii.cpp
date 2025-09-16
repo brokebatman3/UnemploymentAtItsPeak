1d0da1bbb0fa63e8a389e8d35d8c41ff73f56875
BrokeBatman <brokebatman3@gmail.com>
2025-09-16 11:32:20 +0530

    Time: 3 ms (3.02%), Space: 17.9 MB (54.86%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool seahrch(vector<int>& nums, int target) {
+    bool search(vector<int>& nums, int target) {
         if (nums.size() == 0) {
             return 0;
         }
@@ -14,7 +14,7 @@ public:
             int mid = l + (h - l) / 2;
             cout << mid << " ";
             if (target == nums[mid]) {
-                return mid;
+                return 1;
             }
             if (nums[l] == nums[mid] && nums[mid] == nums[h]) {
                 l++;
@@ -37,40 +37,5 @@ public:
         }
         return 0;
     }
-    bool search(vector<int>& arr, int key) {
-        int n = arr.size();
-        if (n == 0) return false;
-
-        int l = 0, h = n - 1;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            cout << mid << " ";
-            if (arr[mid] == key) return true;
-
-            // Ambiguous case due to duplicates: shrink both sides
-            if (arr[l] == arr[mid] && arr[mid] == arr[h]) {
-                l++;
-                h--;
-                continue;
-            }
-
-            // If left half is sorted
-            if (arr[l] <= arr[mid]) {
-                if (key >= arr[l] && key < arr[mid]) {
-                    h = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
-            }
-            // Otherwise right half is sorted
-            else {
-                if (key > arr[mid] && key <= arr[h]) {
-                    l = mid + 1;
-                } else {
-                    h = mid - 1;
-                }
-            }
-        }
-        return false;
-    }
+  
 };
